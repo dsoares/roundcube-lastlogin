c01beecc2213ed2c47e7e5844947d4bd91d8c51f
dsoares <diana.soares@gmail.com>
2018-05-02 17:58:23 +0100

    Release version 0.1.14

--- lastlogin.php
+++ lastlogin.php
@@ -4,7 +4,7 @@
  *
  * Roundcube plugin to provide information (IP, DNS, Geo) about the user last logins.
  *
- * @version 0.1.13
+ * @version 0.1.14
  * @author Diana Soares
  * @requires geolocation
  *
